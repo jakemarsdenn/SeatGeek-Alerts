7bdf837c43c3e9ed5121cab4fc078d55f38a0313
Jake <jmar7284@uni.sydney.edu.au>
2024-04-03 16:23:24 -0700

    created event search bar

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/search_for_event', methods=['POST'])
+def search_for_event():
+    event_name = request.form.get('inputField')
+    return render_template('events.html', event_name=event_name)
+
 if __name__ == '__main__':
     app.run(debug=True)
