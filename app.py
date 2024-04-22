797f26ded3afbb7e95ff9145847cac0ab0c9bfb8
Jake <jmar7284@uni.sydney.edu.au>
2024-04-21 23:06:34 -0700

    implemented events-backend

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect
 from account import valid_account, valid_credentials, get_name
+from events import get_events
 
 
 app = Flask(__name__)
@@ -74,18 +75,8 @@ def edit_profile():
 @app.route('/search_for_event', methods=['POST'])
 def search_for_event():
     event_name = request.form.get('inputField')
-    # top_events = get_events_js(event_name)
-    return render_template('events.html', session=session, event_name=event_name)
-    # return render_template('events.html', event_name=event_name, top_events=top_events)
-
-
-def get_events_js(event_name):
-    return None
-    # script_path = 'static/js/main.js'
-    # if os.path.exists(script_path):
-    #     with open(script_path, 'r') as f:
-    #         js_code = f.read()
-    #         print("JavaScript file located successfully.")
+    events = get_events(event_name)
+    return render_template('events.html', session=session, event_name=event_name, events=events)
 
 
 if __name__ == '__main__':
